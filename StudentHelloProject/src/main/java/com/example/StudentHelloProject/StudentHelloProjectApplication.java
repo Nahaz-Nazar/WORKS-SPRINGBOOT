5980d96a00a9ac7f6488e985ff08266f1dd06040
nahaz <nahaznazar73@gmail.com>
2026-09-10 22:29:51 +0530

    Added Spring Boot project with basic structure and configurations

---
+++ StudentHelloProject/src/main/java/com/example/StudentHelloProject/StudentHelloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentHelloProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHelloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHelloProjectApplication.class, args);
+	}
+
+}
